063d4760643678bfd453652b4fff0fc92d7f129d
BotS1lent <tom.laumonier@gmail.com>
2020-04-16 18:38:41 +0400

    Update cc_morpion.py

--- cc_morpion.py
+++ cc_morpion.py
@@ -12,7 +12,7 @@ C8 = ['C8']
 M1 = C0 + C1 + C2
 M2 = C3 + C4 + C5
 M3 = C6 + C7 + C8
-game = M1 + M2 + M3
+game = M1 + M2 + M3	
 print(M1)
 print(M2)
 print(M3)
@@ -21,13 +21,25 @@ print(M3)
 while i!=9 :
 	print("Au tour des croix !")
 	X = input('')
-	while X 
-
+	if X in game :
+		if X in M1 :
+			A = M1.index(X)
+			M1[A]="X"
+		
+		elif X in M2 :
+			A = M2.index(X)
+			M2[A]="X"
+			del home[A]
+		elif X in M3 :
+			A = M3.index(X)
+			M3[A]="X"
+			del home[A]
+		print(game)
 		print(M1)
 		print(M2)
 		print(M3)
 	else :
-		print("TRICHEUR, réessaie")
+		print("TRICHEUR, saute ton tour")
 	i += 1
 	if i == 9 :
 		break
@@ -35,11 +47,21 @@ while i!=9 :
 	print("Au tour des cercles!")
 	O = input('')
 	if O in game :
-		del game[O]
+		if O in M1 :
+			A = M1.index(O)
+			M1[A]="O"
+		if O in M2 :
+			A = M2.index(O)
+			M2[A]="O"
+		if O in M3 :
+			A = M3.index(O)
+			M3[A]="O"
+		print(game)
 		print(M1)
 		print(M2)
 		print(M3)
-
+	else :
+		print("TRICHEUR, saute ton tour")
 	i += 1
 	if i == 9 :
 		break
